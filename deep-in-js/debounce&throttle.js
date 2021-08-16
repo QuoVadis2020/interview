b953e0f60abfc1b91be1478b6efc8b5a8608a5d8
黄海靖 <im.mirror@live.cn>
2021-08-16 15:24:44 +0800

    fix

--- deep-in-js/debounce&throttle.js
+++ deep-in-js/debounce&throttle.js
@@ -2,8 +2,21 @@
  * 防抖和节流都是在指定时间间隔内防止函数多次调用
  */
 
-// 防抖是只执行一次任务
-function throttle(fn, delay = 300) {
+// 防抖：动作发生后一定时间后触发事件，在这段时间内，如果该动作又发生，则重新等待一定时间再触发事件。
+// 回城
+const debounce = (fn, delay = 300) => {
+    let timer
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout((...args) => {
+            fn.apply(this, args)
+        })
+    }
+}
+
+// 节流：动作发生后一段时间后触发事件，在这段时间内，如果动作又发生，则无视该动作，直到事件执行完后，才能重新触发
+// 技能CD
+const throttle = (fn, delay = 300) => {
     let start = Date.now()
     let now
     let timer
@@ -18,16 +31,5 @@ function throttle(fn, delay = 300) {
                 fn.apply(this, args)
             }, delay)
         }
-    }
-}
-
-// 节流是将多次执行变为最后一次执行
-const debounce = (fn, delay = 300) => {
-    let timer
-    return () => {
-        clearTimeout(timer)
-        timer = setTimeout((...args) => {
-            fn.apply(this, args)
-        })
     }
 }
